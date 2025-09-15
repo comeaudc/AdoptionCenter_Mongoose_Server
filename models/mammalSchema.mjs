543835e78ca3e9e7bc3bc1a9886ca6cef48e8ebf
DylanC <comeaudc@comcast.net>
2025-09-15 13:55:05 -0400

    Added Indexes, virtuals and methods & routes to use them

--- models/mammalSchema.mjs
+++ models/mammalSchema.mjs
@@ -22,4 +22,25 @@ const mammalSchema = new mongoose.Schema({
   },
 });
 
+// Create an Index for faster querying
+mammalSchema.index({ species: 1 });
+mammalSchema.index({ habitat: 1 });
+
+// Create status methods to get Avg age of all animals in the adoptionCenter
+mammalSchema.statics.inHabitat = function (habitat) {
+  return this.find({ habitat: habitat });
+};
+
+// Instance method - used on an INSTANCE of the collection
+mammalSchema.methods.getOthersInHab = function (cb) {
+  return mongoose.model("Mammal").find({ habitat: this.habitat });
+};
+
+// Virtual Value
+mammalSchema.virtual("old").get(function () {
+  return this.age > 10;
+});
+// Setting virtuals to true
+mammalSchema.set("toJSON", { virtuals: true });
+
 export default mongoose.model("Mammal", mammalSchema);
--- routes/mammalRoutes.mjs
+++ routes/mammalRoutes.mjs
@@ -2,6 +2,27 @@ import express from "express";
 import Mammal from "../models/mammalSchema.mjs";
 const router = express.Router();
 
+// Get animals by habitat
+router.get("/habitat/:hab", async (req, res) => {
+  let result = await Mammal.inHabitat(req.params.hab);
+
+  res.json(result);
+});
+
+// Get animals with a similar habitat
+router.get("/eco/:id", async (req, res) => {
+  try {
+    let animal = await Mammal.findById(req.params.id);
+
+    let result = await animal.getOthersInHab();
+
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: `❌ Error - ${err.message}` });
+  }
+});
+
 // Create
 router
   .route("/")
@@ -30,7 +51,32 @@ router
   });
 
 // Update
+router
+  .route("/:id")
+  .put(async (req, res) => {
+    try {
+      let updatedMammal = await Mammal.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true } // Option to allow newly updated object to be sent back
+      );
 
-// Delete
+      res.json(updatedMammal);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: `❌ Error - ${err.message}` });
+    }
+  })
+  // Deleted
+  .delete(async (req, res) => {
+    try {
+      let deleteMammal = await Mammal.findByIdAndDelete(req.params.id);
+
+      res.json(deleteMammal);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: `❌ Error - ${err.message}` });
+    }
+  });
 
 export default router;
