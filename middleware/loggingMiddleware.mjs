b790333329c83cdffa1f613f8b0616243bb714e7
DylanC <comeaudc@comcast.net>
2025-09-15 12:00:04 -0400

    Create & Read

--- middleware/loggingMiddleware.mjs
+++ middleware/loggingMiddleware.mjs
@@ -1,3 +1,5 @@
-export default (req, res, next) =>{
-    console.log(`${req.method} - ${req.path}`)
-}
+export default (req, res, next) => {
+  console.log(`${req.method} - ${req.path}`);
+
+  next();
+};
--- routes/mammalRoutes.mjs
+++ routes/mammalRoutes.mjs
@@ -1,9 +1,36 @@
-import express from 'express';
-import Mammal from '../models/mammalSchema.mjs';
+import express from "express";
+import Mammal from "../models/mammalSchema.mjs";
 const router = express.Router();
 
+// Create
+router
+  .route("/")
+  .post(async (req, res) => {
+    try {
+      // Perform Action
+      let newMammal = await Mammal.create(req.body);
 
+      // Return Response
+      res.json(newMammal);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: `❌ Error - ${err.message}` });
+    }
+  })
+  // Read
+  .get(async (req, res) => {
+    try {
+      let allMammals = await Mammal.find({});
 
+      res.json(allMammals);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: `❌ Error - ${err.message}` });
+    }
+  });
 
+// Update
+
+// Delete
 
 export default router;
